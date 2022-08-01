5b95a6bc7ef459991f566ea8fb2ccdae64ee4cb4
omri levi <omrilevi1994@gmail.com>
2022-08-01 15:29:49 +0300

    port 3000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 const cors = require('cors');
 const http = require('http');
 var app = express(),
